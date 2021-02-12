b43b97539c5c9376730203dc1838216fa57bc3e2
Zsolt Szakal <zsltszakal@gmail.com>
2021-02-12 09:21:32 +0000

    added function to form

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+
+    })
+
+    function handleChange(event){
+     const {name, value}   = event.target
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            }
+        })
+    }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
         <button>+</button>
       </form>
     </div>
