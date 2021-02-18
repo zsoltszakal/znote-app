356d67e5a5770fa6696b8f2e615480a2bbd05c81
Zsolt Szakal <60626421+zsoltszakal@users.noreply.github.com>
2021-02-18 16:32:52 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 function Header () {
     return (
         <div>
-            <h1 className="header header-text">&nbsp;&nbsp;zNote&nbsp;<a className="github" href="https://github.com/zsoltszakal/znote-app" target="_blank" rel="noopener noreferrer"><GitHubIcon /></a></h1>
+            <h1 className="header header-text">zNote&nbsp;<a className="github" href="https://github.com/zsoltszakal/znote-app" target="_blank" rel="noopener noreferrer"><GitHubIcon /></a></h1>
         </div>
     );
 
